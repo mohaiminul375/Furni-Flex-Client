656d7fdc774b71e54986218c7cb87aba3374484b
Mohaiminul375 <mohaiminul375@gmail.com>
2024-09-06 17:21:48 +0600

    update: respinsive footer

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import logo from "../../../src/assets/icon.png";
 import { Link } from "react-router-dom";
 import social from '../../../src/assets/social icons.png'
+import us_flag from "../../../src/assets/us-flag.png"
 const Footer = () => {
   return (
-    <footer className="mt-20 bg-[#0E0E0E] h-[500px] px-10 py-10 text-white">
+    <footer className="mt-20 bg-[#0E0E0E] px-10 py-10 text-white">
       <section className="grid md:grid-cols-4">
         {/* logo */}
         <div>
@@ -16,9 +17,9 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="text-center">
+        <div className="text-left">
           <h3 className="font-bold text-xl mb-5">About Us</h3>
-          <div className="flex flex-col justify-center items-center space-y-2">
+          <div className="flex flex-col justify-center items-start space-y-2">
             <Link className="text-[#81859F]" to="/">
               Master Plan
             </Link>
@@ -39,9 +40,9 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="text-center">
+        <div className="text-left">
           <h3 className="font-bold text-xl mb-5">Explore EEvE</h3>
-          <div className="flex flex-col justify-center items-center space-y-2">
+          <div className="flex flex-col justify-center items-start space-y-2">
             <Link className="text-[#81859F]" to="/">
               Unlock My Robot Power
             </Link>
@@ -56,9 +57,9 @@ const Footer = () => {
             </Link>
           </div>
         </div>
-        <div className="text-center">
+        <div className="text-left">
           <h3 className="font-bold text-xl mb-5">Community&Support</h3>
-          <div className="flex flex-col justify-center items-center space-y-2">
+          <div className="flex flex-col justify-center items-start space-y-2">
             <Link className="text-[#81859F]" to="/">
               Willow X Community
             </Link>
@@ -74,15 +75,15 @@ const Footer = () => {
           </div>
         </div>
       </section>
-      <hr />
-      <section className="mt-6">
-        <div className="flex flex-col md:flex-row justify-between">
+      <hr className="my-5"/>
+      <section className="">
+        <div className="flex flex-col md:flex-row justify-between gap-5 md:gap-0">
             {/*left */}
             <div>
                  <img src={social} alt="" />
             </div>
             {/* middle */}
-            <div className="flex justify-between gap-5">
+            <div className="flex flex-col md:flex-row justify-between gap-5 text-[#81859F]">
                <p>March22 Recap</p>
                <p>Private Policy</p>
                <p>General Terms</p>
@@ -90,11 +91,11 @@ const Footer = () => {
                </p>
             </div>
             {/* right */}
-            <div>
-                <p>United State(English)</p>
+            <div className="text-[#81859F]">
+                <p className="flex items-center gap-1"><img src={us_flag} alt='us-flag' />United State(English)</p>
             </div>
         </div>
-        <p className="text-center">EEVE © 2024. All rights reserved.</p>
+        <p className="md:text-center mt-5 text-[#81859F]">EEVE © 2024. All rights reserved.</p>
       </section>
     </footer>
   );
