b07c53b0625d1b23d43660b4fbd88dfcb677c2eb
Mohaiminul375 <mohaiminul375@gmail.com>
2024-09-06 19:08:52 +0600

    add: increase and decrease quantity

--- src/Provider/CartProvider.jsx
+++ src/Provider/CartProvider.jsx
@@ -9,7 +9,7 @@ const CartProvider = ({ children }) => {
       const productExisted = prevCart?.find((item) => item._id === product._id);
       if (productExisted) {
         return prevCart.map((item) =>
-          item._id === prevCart._id
+          item._id === product._id
             ? { ...item, quantity: item.quantity + 1 }
             : item
         );
@@ -21,8 +21,16 @@ const CartProvider = ({ children }) => {
 
   // remove from cart
   const removeFromCart = () => {};
+  // updateQuantity
+  const updateQuantity = (productId, quantity) => {
+    setCart((prevCart) => 
+        prevCart.map(item =>
+            item._id == productId ? { ...item, quantity: quantity } : item
+        )
+    );
+};
   return (
-    <CartContext.Provider value={{ cart, addToCart }}>
+    <CartContext.Provider value={{ cart, addToCart,updateQuantity }}>
       {children}
     </CartContext.Provider>
   );
